10fb6c95c15affd65baa791b68bebd97b858336e
ezagoc <ezagocue@itam.mx>
2026-06-10 19:03:47 -0400

    Add: missing ads files

--- code/02-analysis/graphs/plot_saved_results_only.R
+++ code/02-analysis/graphs/plot_saved_results_only.R
@@ -1,7 +1,35 @@
 # 0.0 Set up the environment, clean it and set working directory to the code path
 rm(list = ls())
-rstudioapi::getActiveDocumentContext
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+resolve_script_dir <- function() {
+  rstudio_path <- tryCatch(
+    {
+      if (requireNamespace("rstudioapi", quietly = TRUE)) {
+        rstudioapi::getActiveDocumentContext()$path
+      } else {
+        ""
+      }
+    },
+    error = function(...) ""
+  )
+
+  if (nzchar(rstudio_path)) {
+    return(dirname(normalizePath(rstudio_path, winslash = "/", mustWork = TRUE)))
+  }
+
+  file_arg <- grep("^--file=", commandArgs(trailingOnly = FALSE), value = TRUE)
+
+  if (length(file_arg) > 0) {
+    script_path <- sub("^--file=", "", file_arg[[1]])
+    if (file.exists(script_path)) {
+      return(dirname(normalizePath(script_path, winslash = "/", mustWork = TRUE)))
+    }
+  }
+
+  normalizePath(getwd(), winslash = "/", mustWork = TRUE)
+}
+
+setwd(resolve_script_dir())
 
 # 1.0 Import packages
 ipak <- function(pkg) {
--- code/02-analysis/graphs/run_plot_saved_results_only.R
+++ code/02-analysis/graphs/run_plot_saved_results_only.R
@@ -1,7 +1,34 @@
-﻿rm(list = ls())
-rstudioapi::getActiveDocumentContext
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rm(list = ls())
+
+resolve_script_dir <- function() {
+  rstudio_path <- tryCatch(
+    {
+      if (requireNamespace("rstudioapi", quietly = TRUE)) {
+        rstudioapi::getActiveDocumentContext()$path
+      } else {
+        ""
+      }
+    },
+    error = function(...) ""
+  )
+
+  if (nzchar(rstudio_path)) {
+    return(dirname(normalizePath(rstudio_path, winslash = "/", mustWork = TRUE)))
+  }
+
+  file_arg <- grep("^--file=", commandArgs(trailingOnly = FALSE), value = TRUE)
+
+  if (length(file_arg) > 0) {
+    script_path <- sub("^--file=", "", file_arg[[1]])
+    if (file.exists(script_path)) {
+      return(dirname(normalizePath(script_path, winslash = "/", mustWork = TRUE)))
+    }
+  }
+
+  normalizePath(getwd(), winslash = "/", mustWork = TRUE)
+}
+
+setwd(resolve_script_dir())
 
 message("Rebuilding paper-facing plots from saved results")
 source("plot_saved_results_only.R")
-
